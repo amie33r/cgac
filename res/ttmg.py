dc66af6a0fab560a12ea228b06fa62c7a7803e0e
amie33r <75431883+amie33r@users.noreply.github.com>
2020-12-04 14:34:06 +0530

    Update ttmg.py

--- res/ttmg.py
+++ res/ttmg.py
@@ -1,18 +1,15 @@
-import os, time, uuid, re, IPython, sys
-import json
-import ipywidgets as widgets
+import os
 from sys import exit as exx
+import time
+import uuid
+import re
 from subprocess import Popen,PIPE
-from google.colab import files  # pylint: disable=import-error
-from glob import glob
-from IPython.display import HTML, display, clear_output
 
 HOME = os.path.expanduser("~")
 CWD = os.getcwd()
 
 # All found ngrok authtoken from github
 tokens = {
-    "lostcatbox": "1f9tD7nftFEYssYeWFaP5ZO5tkL_5kaPxXbBJZT399uFXBkV7",
     "lostcatbox": "1X7aYWPuFKYzvewLbnNoMo71kZi_2uzbB966Q4TU5cpgNPKhy",
     "zero-structure": "1UqHsShi6o3ketf426P5UtVdTfs_5XFD6sFRMkryka8fAbLd3",
     "ekkleesia": "7LE18LK8zeaDYeybp5fKP_6GNG1oHEfhTnQE7s9qpw",
@@ -33,7 +30,6 @@ tokens = {
     "api1": "6qGnEsrCL4GqZ7hMfqpyz_7ejAThUCjVnU9gD5pbP5u",
     "api2": "1Q4i7F6isO7zZRrrjBKZzZhwsMu_74yJqoEs1HrJh1zYyxNo1",
     "api3": "1SCsbuawjv9d79jlhlfNljaFTaB_5heVkcR6C7Sk8UBaQ1U1C",
-    "api4": "1f9tD7nftFEYssYeWFaP5ZO5tkL_5kaPxXbBJZT399uFXBkV7",
     "api5": "1Q45NXgsx6oyusN3GiNAYvkNJPS_AveYUDBcPHsvRvf21WZv",
     "api6": "1Q6smHt4Bzz9VEXTwj3a7p5Gdx2_5mp6ivT6N6nB3YmRHUEM3",
     "api7": "7VJwGkCTTUubiGhgz6Gv6_5fMLganRSKj9ntdefnF5o",
@@ -222,48 +218,6 @@ class ngrok:
         if dati == True:
             continue
         return dati
- # ====================================================================================================
-def createButton(name, *, func=None, style="", icon="check"):
-    import ipywidgets as widgets  # pylint: disable=import-error
-
-    button = widgets.Button(
-        description=name, button_style=style, icon=icon, disabled=not bool(func)
-    )
-    button.style.font_weight = "900"
-    button.on_click(func)
-    output = widgets.Output()
-    display(button, output)
-
-def generateRandomStr():
-    from uuid import uuid4
-
-    return str(uuid4()).split("-")[0]
-
-def accessSettingFile(file="", setting={}):
-    from json import load, dump
-
-    if not isinstance(setting, dict):
-        print("Only accept Dictionary object.")
-        exx()
-    fullPath = f"/usr/local/sessionSettings/{file}"
-    try:
-        if not len(setting):
-            if not checkAvailable(fullPath):
-                print(f"File unavailable: {fullPath}.")
-                exx()
-            with open(fullPath) as jsonObj:
-                return load(jsonObj)
-        else:
-            with open(fullPath, "w+") as outfile:
-                dump(setting, outfile)
-    except:
-        print(f"Error accessing the file: {fullPath}.")
-
-def memGiB():
-    from os import sysconf as _sc  # pylint: disable=no-name-in-module
-
-    return _sc("SC_PAGE_SIZE") * _sc("SC_PHYS_PAGES") / (1024.0 ** 3)
-# ====================================================================================================
 
 def checkAvailable(path_="", userPath=False):
     from os import path as _p
@@ -420,49 +374,6 @@ def textAn(TEXT, ty='d'):
             textcover = str(len(TEXT)*0.55)
             return display(HTML('''<style>@import url(https://fonts.googleapis.com/css?family=Anonymous+Pro);.line-1{font-family: 'Anonymous Pro', monospace;    position: relative;   border-right: 1px solid;    font-size: 15px;   white-space: nowrap;    overflow: hidden;    }.anim-typewriter{  animation: typewriter 0.4s steps(44) 0.2s 1 normal both,             blinkTextCursor 600ms steps(44) infinite normal;}@keyframes typewriter{  from{width: 0;}  to{width: '''+textcover+'''em;}}@keyframes blinkTextCursor{  from{border-right:2px;}  to{border-right-color: transparent;}}</style><div class="line-1 anim-typewriter">'''+TEXT+'''</div>'''))
 
-def updateCheck(self, Version):
-    class UpdateChecker(object):
-
-      def __init__(self):
-          getMessage = self.getMessage
-          getVersion = self.getVersion
-
-      def getVersion(self, currentTag):
-          from urllib.request import urlopen
-          from lxml.etree import XML
-
-          url = self.URL
-          update = urlopen(url).read()
-          root = XML(update)
-          cur_version = root.find(".//"+currentTag)
-          current = cur_version.text
-          return current
-
-      def getMessage(self, messageTag):
-          from urllib.request import urlopen
-          from lxml.etree import XML
-
-          url = self.URL
-          update = urlopen(url).read()
-          root = XML(update)
-          mess = root.find(".//"+messageTag)
-          message = mess.text
-          return message
-
-    check = UpdateChecker()
-    check.URL = "https://raw.githubusercontent.com/amie33r/cgac/master/update.xml"
-    currentVersion = check.getVersion("currentVersion")
-    message = check.getMessage("message")
-
-    if Version != currentVersion:
-        from IPython.display import HTML
-
-        print("Script Update Checker: Version "+currentVersion+" "+message+" Your version: "+Version+"")
-        display(HTML('<div style="background-color: #4caf50!important;text-align: center;padding-top:-1px;padding-bottom: 9px;boder:1px"><h4 style="padding-top:5px"><a target="_blank" href="http://bit.ly/updateCscript" style="color: #fff!important;text-decoration: none;color: inherit;background-color:transparent;font-family: Segoe UI,Arial,sans-serif;font-weight: 400;font-size: 20px;">Open Latest Version</a></h4></div>'))
-        return True
-    else:
-        print("Script Update Checker: Your script is up to date")
-
 class LocalhostRun:
   def __init__(self,port,id=None,interval=30,retries=30):
     import os
@@ -501,16 +412,25 @@ class LocalhostRun:
         return oldAddr
       except:
         pass
-
-    self.connection=Popen(f"autossh -R 80:localhost:{self.port} {self.id}@ssh.localhost.run -o StrictHostKeyChecking=no -o ServerAliveInterval={self.interval} -o ServerAliveCountMax={self.retries}".split(), stdout=PIPE, stdin=PIPE)
-    #print("ssh -R 80:localhost:{self.port} {self.id}@ssh.localhost.run -o StrictHostKeyChecking=no -o ServerAliveInterval={self.interval} -o ServerAliveCountMax={self.retries}")
-    try:
-      newAddr = re.findall("http://(.*?.localhost.run)",self.connection.stdout.readline().decode("utf-8"))[0]
-      localhostOpenDB[str(self.port)] = newAddr 
-      accessSettingFile("localhostDB.json" , localhostOpenDB, v=False)
-      return newAddr
-    except:
-      raise Exception(self.connection.stdout.readline().decode("utf-8"))
+    for _ in range(2):
+      self.connection=Popen(f"autossh -R 80:localhost:{self.port} {self.id}@ssh.localhost.run -o StrictHostKeyChecking=no -o ServerAliveInterval={self.interval} -o ServerAliveCountMax={self.retries}".split(),
+        stdout=PIPE, stdin=PIPE, stderr=PIPE)
+      #print("ssh -R 80:localhost:{self.port} {self.id}@ssh.localhost.run -o StrictHostKeyChecking=no -o ServerAliveInterval={self.interval} -o ServerAliveCountMax={self.retries}")
+      try:
+        newAddr = re.findall("http://(.*?.localhost.run)", self.connection.stdout.readline().decode("utf-8"))[0]
+        localhostOpenDB[str(self.port)] = newAddr 
+        accessSettingFile("localhostDB.json" , localhostOpenDB, v=False)
+        return newAddr
+      except:
+        outs, errs = self.connection.communicate(timeout=15)
+        self.connection.kill()
+        # print(outs)
+        # print(errs)
+        if re.search(r"Permission\sdenied\s\(publickey\)", errs.decode("utf-8")):
+          os.system("ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa")
+          continue
+        raise Exception(errs.decode("utf-8"))
+      break
 
   def kill(self):
     self.connection.kill()
